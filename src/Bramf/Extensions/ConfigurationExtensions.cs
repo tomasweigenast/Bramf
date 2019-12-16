a610f156581e10fe2d9bf1e238758f52f54ba0f6
Tomas Wegenast <tomaswegenast@gmail.com>
2019-12-16 11:01:13 -0300

    Added ConfigurationExtensions

---
+++ src/Bramf/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Bramf.Extensions
+{
+    /// <summary>
+    /// Extension methods to work with Microsoft <see cref="IConfiguration"/>
+    /// </summary>
+    public static class ConfigurationExtensions
+    {
+        /// <summary>
+        /// Gets a boolean from the configuration
+        /// </summary>
+        /// <param name="configuration">The configuration source.</param>
+        /// <param name="key">The key.</param>
+        public static bool GetBool(this IConfiguration configuration, string key)
+            => Get<bool>(configuration, key);
+
+        /// <summary>
+        /// Gets a double from the configuration
+        /// </summary>
+        /// <param name="configuration">The configuration source.</param>
+        /// <param name="key">The key.</param>
+        public static double GetDouble(this IConfiguration configuration, string key)
+            => Get<double>(configuration, key);
+
+        /// <summary>
+        /// Gets a DateTime from the configuration
+        /// </summary>
+        /// <param name="configuration">The configuration source.</param>
+        /// <param name="key">The key.</param>
+        public static DateTime GetDateTime(this IConfiguration configuration, string key)
+        {
+            string value = configuration[key];
+            if (!DateTime.TryParse(value, out DateTime result))
+                throw new FormatException("Invalid DateTime format.");
+
+            return result;
+        }
+
+        /// <summary>
+        /// Gets a float from the configuration
+        /// </summary>
+        /// <param name="configuration">The configuration source.</param>
+        /// <param name="key">The key.</param>
+        public static float GetFloat(this IConfiguration configuration, string key)
+            => Get<float>(configuration, key);
+
+        /// <summary>
+        /// Gets a long from the configuration
+        /// </summary>
+        /// <param name="configuration">The configuration source.</param>
+        /// <param name="key">The key.</param>
+        public static long GetLong(this IConfiguration configuration, string key)
+            => Get<long>(configuration, key);
+
+        /// <summary>
+        /// Gets an int from the configuration
+        /// </summary>
+        /// <param name="configuration">The configuration source.</param>
+        /// <param name="key">The key.</param>
+        public static int GetNumber(this IConfiguration configuration, string key)
+            => Get<int>(configuration, key);
+
+        private static T Get<T>(IConfiguration configuration, string key)
+        {
+            string value = configuration[key];
+            try
+            {
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+            catch
+            {
+                throw new FormatException("Invalid value type.");
+            }
+        }
+    }
+}
