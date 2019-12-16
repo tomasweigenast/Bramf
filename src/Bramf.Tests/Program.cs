f401dd2813bdd30f618fe7defabe5d5f26903ff0
Tomas Wegenast <tomaswegenast@gmail.com>
2019-12-16 19:47:04 -0300

    Added Tests

---
+++ src/Bramf.Tests/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bramf.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<Entity, string> obj = x => x.Id;
+            Delegate[] list = obj.GetInvocationList();
+            Type t = obj.GetType();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Entity
+    {
+        public string Id => Guid.NewGuid().ToString();
+    }
+}
