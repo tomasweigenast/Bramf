b3755f799c42c3f10e8588b936d64aadfe88bceb
Tomas Wegenast <tomaswegenast@gmail.com>
2019-12-17 16:29:29 -0300

    added Byte[] helpers

--- src/Bramf/Extensions/ArrayExtensions.cs
+++ src/Bramf/Extensions/ArrayExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Bramf.Extensions
 {
@@ -26,5 +28,39 @@ namespace Bramf.Extensions
         /// <returns></returns>
         public static T[] Prepend<T>(this T[] source, params T[] toAdd)
             => toAdd.Append(source);
+
+        /// <summary>
+        /// Combines two byte arrays
+        /// </summary>
+        /// <param name="left">The first byte[]</param>
+        /// <param name="right">The second byte[]</param>
+        public static byte[] CombineWith(this byte[] left, byte[] right)
+        {
+            byte[] output = new byte[left.Length + right.Length];
+            Buffer.BlockCopy(left, 0, output, 0, left.Length);
+            Buffer.BlockCopy(right, 0, output, left.Length, right.Length);
+
+            return output;
+        }
+
+        /// <summary>
+        /// Checks if two byte[] are equals or not
+        /// </summary>
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public static bool IsEqualsTo(this byte[] left, byte[] right)
+        {
+            if (ReferenceEquals(left, right))
+                return true;
+
+            if (left == null || right == null || left.Length != right.Length)
+                return false;
+
+            bool areSame = true;
+            for (int i = 0; i < left.Length; i++)
+                areSame &= (left[i] == right[i]);
+
+            return areSame;
+        }
+
     }
 }
