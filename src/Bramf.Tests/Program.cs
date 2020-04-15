c68cb3f76d43173ddb7cc42cdf29cb7dc20d5277
Tomas Wegenast <tomaswegenast@gmail.com>
2020-04-15 20:07:19 -0300

    Remove nuget packages

--- src/Bramf.Tests/Program.cs
+++ src/Bramf.Tests/Program.cs
@@ -1,4 +1,4 @@
-﻿using Bramf.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System;
 
 namespace Bramf.Tests
@@ -7,41 +7,11 @@ namespace Bramf.Tests
     {
         static void Main(string[] args)
         {
-            ConfigurationEnvironment configuration = new ConfigurationEnvBuilder(Environment.GetFolderPath(Environment.SpecialFolder.Desktop))
-                .AddProvider<Entity>()
-                .AddProvider<EntityEncrypted>(x =>
-                {
-                    x.Encrypt = true;
-                    x.Load = false;
-                })
-                .Build();
-
-            Console.WriteLine($"Value: {configuration.Get<EntityEncrypted>().Id}");
-
-            configuration.BeginEdit<EntityEncrypted>(x =>
-            {
-                x.Id = "hello world";
-            });
-
-            Console.WriteLine($"Value: {configuration.Get<EntityEncrypted>().Id}");
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .BuildServiceProvider();
 
             Console.ReadLine();
         }
-    }
-
-    [Config]
-    public class Entity
-    {
-        public string Id { get; set; } = "Mi id";
-
-        public bool Enabled { get; set; }
-    }
-
-    [Config]
-    public class EntityEncrypted
-    {
-        public string Id { get; set; } = "Mi id";
-
-        public bool Enabled { get; set; }
     }
 }
